0b2c8efc5b94c7d2ff49f70a0d7842a6657d8d2d
Marilise Morona <marilise.morona@zarpsystem.com.br>
2023-06-01 18:13:28 -0300

    Create a project

---
+++ TodayApp/TodayApp/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  TodayApp
+//
+//  Created by Marilise Morona on 01/06/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = ViewController()
+        let navigation = UINavigationController(rootViewController: vc)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = navigation
+        return true
+    }
+    
+}
---
+++ TodayApp/TodayApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodayApp
+//
+//  Created by Marilise Morona on 01/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
